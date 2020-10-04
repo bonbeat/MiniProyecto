892cc9734f9468767953416a920ea66145a33412
bonbeat <mariolauro9@gmail.com>
2020-10-03 20:51:48 -0400

    Add project files.

---
+++ MiniProyecto/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MiniProyecto.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+        }
+    }
+}
